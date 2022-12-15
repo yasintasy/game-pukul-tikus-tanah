6af2916d2e34c8e0121a7f6dfc09b24b0c1fa491
Yasinta <yasinta.syara@gmail.com>
2022-12-16 00:23:53 +0700

    done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const tanah = document.querySelectorAll('.tanah');
 const tikus = document.querySelectorAll('.tikus');
-const mulai = document.querySelectorAll('.mulai');
+const papanSkor = document.querySelector('.papan-skor');
+const pop = document.querySelector('#pop');
 
 let tanahSebelumnya;
+let selesai;
+let skor;
 
 function randomTanah(tanah) {
     const t = Math.floor(Math.random() * tanah.length);
@@ -18,13 +21,36 @@ function randomWaktu(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
-function munculkanTikus(tanah) {
+function munculkanTikus() {
     const tRandom = randomTanah(tanah);
     const wRandom = randomWaktu(300, 1000);
     tRandom.classList.add('muncul');
 
     setTimeout(() => {
         tRandom.classList.remove('muncul');
-        munculkanTikus();
+        if (!selesai) {
+            munculkanTikus();
+        }
     }, wRandom);
 }
+
+function mulai() {
+    selesai = false;
+    skor = 0;
+    papanSkor.textContent = 0;
+    munculkanTikus();
+    setTimeout(() => {
+        selesai = true;
+    }, 10000);
+}
+
+function pukul() {
+    skor++;
+    this.parentNode.classList.remove('muncul');
+    pop.play();
+    papanSkor.textContent = skor;
+}
+
+tikus.forEach(t => {
+    t.addEventListener('click', pukul);
+});
